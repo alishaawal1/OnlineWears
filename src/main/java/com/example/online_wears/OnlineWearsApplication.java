962e3f7df9d5e934fba6523aeb37ccc035f4f6fa
alishaawal1 <aleeshaawall455@gmail.com>
2023-02-24 17:29:25 +0545

    designs of website

---
+++ src/main/java/com/example/online_wears/OnlineWearsApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_wears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWearsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_wears/OnlineWearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_wears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
